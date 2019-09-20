54fa5d317f583bcaa4feb47b6ed17292add7c161
Ilham Gibran <ilgibrz@gmail.com>
2019-09-20 10:02:50 +0700

    Update JS

--- wp-content/themes/orlandotheme/index.php
+++ wp-content/themes/orlandotheme/index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -23,6 +22,7 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+    <?php wp_head();?>
   </head>
 
   <body>
@@ -165,7 +165,8 @@
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <script src="<?php echo get_bloginfo('template_directory'); ?>/jquery.min.js"></script>
+    <script src="<?php echo get_bloginfo('template_directory'); ?>/bootstrap.min.js"></script>
+    <?php wp_footer(); ?>
   </body>
 </html>
